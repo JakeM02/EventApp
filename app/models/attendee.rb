bb723db2496e8f2e9e88c25155465c0cf862d6ac
JakeM02 <jake76@live.ie>
2024-12-20 17:39:12 +0000

    additional comments on views

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,5 +2,5 @@ class Attendee < ApplicationRecord
   belongs_to :event
 
   validates :name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } # ensures email follows standard format with @
 end
